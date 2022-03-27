402a735a30404fe01415f49f05ce90ccc7b7e1b1
Amer <amereid92@gmail.com>
2022-03-28 00:32:50 +0400

    Add tweaks to content

--- AmerEidCV/Sources/Views/CVView.swift
+++ AmerEidCV/Sources/Views/CVView.swift
@@ -28,11 +28,11 @@ public class CVView: UIView {
                        linkedInURL: URL(string: "https://www.linkedin.com/in/amer-eid92/")!,
                        flagImageName: uaeFlagImageName),
      
-     SummaryModel(text: "Lead iOS developer with over 6 years of experience creating and developing complex user focused software applications. Currently leading a team of 5 to create the # 1 music streaming app in MENA with millions of users. Responsible for leading the design and implementation of complex technical projects."),
+     SummaryModel(text: "Lead iOS engineer with over 6 years of experience creating and developing complex user focused software applications. Currently leading a team of 5 to create the # 1 music streaming app in MENA with millions of users. Entrepreneurially trained with a passion for creating things that make people happy."),
      
      HeaderModel(title: "Experience"),
          
-     DetailsModel(titleModel: CVLabelModel(text: "Anghami", font: UIFont.p1FontBold, textColor: .blueLinkColor, url: URL(string: "https://anghami.com")!, underlineColor: .blueLinkColor),
+     DetailsModel(titleModel: CVLabelModel(text: "Anghami", font: UIFont.p1FontBold, textColor: .blueLinkColor, url: URL(string: "https://anghami.com/about")!, underlineColor: .blueLinkColor),
                   subtitle: "Lead iOS Engineer",
                   date: "April 2020 - Present",
                   location: "Abu Dhabi, United Arab Emirates"),
@@ -41,10 +41,10 @@ public class CVView: UIView {
      BulletPointModel(text: "Sole lead developer of the iOS platform which boasts a rating of 4.7 (900K ratings) on the App Store", style: .indented),
      BulletPointModel(text: "Managed a team of 5 people and led development on iOS, tvOS, watchOS, iPadOS, and Carplay"),
      BulletPointModel(text: "Main architect of core features such as the custom Audio Engine, Live Radios, Stories, Music Player, Carplay App, Apple Watch App, Chat, In-App Purchases & Subscriptions, etc"),
-     BulletPointModel(text: "Responsible for automating release and testing pipelines using Fastlane and Jenkins saving 32min/build"),
+     BulletPointModel(text: "Automated and deployed release and testing pipelines using Fastlane and Jenkins saving 32min/build"),
      BulletPointModel(text: "Reduced app hang rate from 200ms/hour to 150ms/hour by utilizing GCD to ensure processor heavy processes such as DB (Realm) read/write transactions and others are off the main thread"),
      BulletPointModel(text: "Reduced average memory usage from 430mb to 330mb by deep diving into memory management and eliminating all forms of memory leaks using memory graph along with other Xcode instruments and tools"),
-     BulletPointModel(text: "Reduced hitch rate from 15ms/sec to 7ms/sec by laying out our dynamic homepage using PinLayout (frames) and caching already calculated dimensions to reduce layout calculation time"),
+     BulletPointModel(text: "Reduced hitch rate from 15ms/sec to 7ms/sec by laying out the homepage using PinLayout (frames) and caching already calculated dimensions to reduce layout calculation time"),
      BulletPointModel(text: "Was part of the core team that led to Anghami's IPO - first ever MENA Tech IPO on NASDAQ"),
      
      DetailsModel(titleModel: CVLabelModel(text: "Anghami", font: UIFont.p1FontBold, textColor: .darkGray, url: nil, underlineColor: nil),
@@ -52,7 +52,7 @@ public class CVView: UIView {
                   date: "June 2017 - April 2020",
                   location: "Beirut, Lebanon"),
      
-     BulletPointModel(text: "Responsible for driving best engineering practices within the team and performing code reviews"),
+     BulletPointModel(text: "Drove best engineering practices within the team and performed code reviews"),
      BulletPointModel(text: "Improved rendering time and scroll performance by over 40% by refactoring UI using IGListKit and PinLayout"),
      BulletPointModel(text: "Spearheaded the conversion of the Video Engine to support HLS video within a week to allow for streaming of the Game of Thrones season 8 premiere for the MENA region"),
      
@@ -72,7 +72,7 @@ public class CVView: UIView {
                   date: "June 2016",
                   location: "Beirut, Lebanon"),
 
-     HeaderModel(title: "Skills & Interests"),
+     HeaderModel(title: "Skills & Projects"),
     
      BulletPointLabelsModel(labelsModels:
                                 [CVLabelModel(text: "Programming Languages:", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
@@ -92,9 +92,8 @@ public class CVView: UIView {
                                  CVLabelModel(text: "PinLayout", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://github.com/layoutBox/PinLayout"), underlineColor: UIColor.blueLinkColor),
                                  CVLabelModel(text: "SnapKit", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://github.com/SnapKit/SnapKit"), underlineColor: UIColor.blueLinkColor),
                                  CVLabelModel(text: "Socket.IO", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://github.com/socketio/socket.io-client-swift"), underlineColor: UIColor.blueLinkColor),
-                                 CVLabelModel(text: "Laravel", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://laravel.com"), underlineColor: UIColor.blueLinkColor),
-                                 CVLabelModel(text: "Mimir", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://github.com/amereid/Mimir"), underlineColor: UIColor.blueLinkColor)]),
-     
+                                 CVLabelModel(text: "Laravel", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://laravel.com"), underlineColor: UIColor.blueLinkColor)]),
+
      BulletPointLabelsModel(labelsModels:
                                 [CVLabelModel(text: "Languages:", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
                                  CVLabelModel(text: "English", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
@@ -102,12 +101,10 @@ public class CVView: UIView {
                                  CVLabelModel(text: "Spanish", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil)]),
      
      BulletPointLabelsModel(labelsModels:
-                                [CVLabelModel(text: "Interests:", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
-                                 CVLabelModel(text: "Reddit", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
-                                 CVLabelModel(text: "Astronomy", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
-                                 CVLabelModel(text: "Entrepreneurship", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
-                                 CVLabelModel(text: "Dota 2", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
-                                 CVLabelModel(text: "Banking", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil)])
+                                [CVLabelModel(text: "Passion Projects:", font: UIFont.p1FontRegular, textColor: .black, url: nil, underlineColor: nil),
+                                 CVLabelModel(text: "Mimir", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://github.com/amereid/Mimir"), underlineColor: UIColor.blueLinkColor),
+                                 CVLabelModel(text: "Mimir Memory Logger", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://github.com/amereid/MimirMemoryLogger"), underlineColor: UIColor.blueLinkColor),
+                                 CVLabelModel(text: "Trivio", font: UIFont.p1FontRegular, textColor: UIColor.blueLinkColor, url: URL(string: "https://apps.apple.com/lb/app/trivio-game/id1465896786"), underlineColor: UIColor.blueLinkColor)])
     ]
     
     init() {
@@ -165,7 +162,7 @@ public class CVView: UIView {
     }
 
     private func setupWaterMarkLabel() {
-        let waterMarkLabel = CVLabel(model: CVLabelModel(text: "I made my CV using my own iOS App Project 💻",
+        let waterMarkLabel = CVLabel(model: CVLabelModel(text: "Created using my own iOS App Project 💻",
                                                          font: UIFont.p1FontRegular,
                                                          textColor: .blueLinkColor,
                                                          url: URL(string: "https://github.com/amereid/amereidcv"),
